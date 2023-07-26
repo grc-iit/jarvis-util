5754ebeb6b879220ae23559e19015a9d0d5b659d
jaime <jcernudagarcia@hawk.iit.edu>
2023-07-25 20:20:55 -0500

    Added support for slurm and mechanisms to define architectures

---
+++ jarvis_util/shell/slurm.py
@@ -0,0 +1,115 @@
+"""
+This module contains various wrappers over typical filesystem commands seen
+in shell scripts. This includes operations such as creating directories,
+changing file permissions, etc.
+"""
+import shutil
+import re
+from jarvis_util.shell.exec import Exec
+from jarvis_util.shell.exec_info import ExecInfo
+from jarvis_util.util.hostfile import Hostfile
+
+"""
+Contains all information needed to start a slurm allocation
+"""
+
+
+class SlurmInfo:
+
+    def __init__(self, nnodes: int, node_list="", partition="", exclusive=True):
+        self.partition = partition
+        self.exclusive = exclusive
+
+        self.nNodes = nnodes
+        self.node_list = node_list
+
+        if self.node_list:
+            nodes_on_list = self.count_expansion(node_list)
+            if nodes_on_list > nnodes:
+                raise Exception("Node list requested more nodes than defined on nnodes")
+
+    @staticmethod
+    def count_expansion(s: str):
+        # Check for range in brackets.
+        match = re.search(r'\[(\d+)-(\d+)\]', s)
+        if match:
+            start, end = map(int, match.groups())
+            return end - start + 1
+
+        # Check for single value without brackets.
+        match = re.search(r'(\d+)(?=[^-]*$)', s)
+        if match:
+            return 1
+
+        # Return 0 if there are no matches.
+        return 0
+
+
+class Slurm:
+    """
+    Create directories + subdirectories.
+    """
+
+    def __init__(self, slurm_info):
+        """
+        Create directories + subdirectories. Does not fail if the dirs
+        already exist.
+
+        :param paths: A list of paths or a single path string.
+        :param exec_info: Info needed to execute the mkdir command
+        """
+        self.command = ""
+        self.nodes = ""
+        self.slurm_info = slurm_info
+        self.hostfile = None
+        if shutil.which('salloc') is None:
+            raise EnvironmentError('salloc not found on the system.')
+
+    def __del__(self):
+        self.exit()
+
+    def allocate(self, exec_info=None):
+        # salloc -n 2 -p compute --exclusive
+        base_cmd = "salloc "
+        if self.slurm_info.nnodes:
+            base_cmd += f'-n {self.slurm_info.nnodes} '
+        if self.slurm_info.node_list:
+            base_cmd += f'--nodelist={self.slurm_info.node_list} '
+        if self.slurm_info.partition:
+            base_cmd += f'-p {self.slurm_info.partition} '
+        if self.slurm_info.exclusive:
+            base_cmd += f'--exclusive '
+        self.command = base_cmd
+        Exec(base_cmd, exec_info)
+
+    def get_nodes(self, exec_info=None) -> list:
+        if exec_info is None:
+            exec_info = ExecInfo(collect_output=True)
+        else:
+            exec_info.collect_output = True
+        node = Exec(f'scontrol show hostname', exec_info)
+        self.nodes = node.stdout
+        return self.nodes
+
+    def get_node_list(self):
+        if not self.nodes:
+            self.get_nodes()
+        return self.nodes.splitlines()
+
+    def get_node_counts(self):
+        if not self.nodes:
+            self.get_nodes()
+        return len(self.nodes.splitlines())
+
+    def get_command(self):
+        return self.command
+
+    def get_hostfile(self):
+        if self.hostfile is None:
+            if not self.nodes:
+                self.get_nodes()
+            self.hostfile = Hostfile(all_hosts=self.get_node_list())
+        return self.hostfile
+
+    def exit(self, exec_info=None):
+        Exec(f'exit', exec_info)
---
+++ jarvis_util/util/architecture.py
@@ -0,0 +1,119 @@
+from jarvis_util import Hostfile
+from jarvis_util.shell.slurm import Slurm, SlurmInfo
+
+
+class Service:
+    def __init__(self, name, num_nodes, color_distribution):
+        self.name = name
+        self.num_nodes = num_nodes
+        self.color_distribution = self.parse_distribution(color_distribution)
+        self.allocated_nodes = []
+        self.hostfile = None
+        self.validate()
+
+    @staticmethod
+    def parse_distribution(distribution):
+        """Parse a color distribution string into a dictionary."""
+        colors = distribution.split("+")
+        color_dict = {}
+        for color in colors:
+            color_split = color.split(":")
+            if len(color_split) == 1:  # only color is specified, no number
+                color_dict[color_split[0]] = None
+            else:
+                color_dict[color_split[0]] = int(color_split[1])
+        return color_dict
+
+    def add_nodes(self, nodes: list):
+        self.allocated_nodes.extend(nodes)
+
+    def add_hostfile(self, nodes: list):
+        self.add_nodes(nodes)
+        self.hostfile = Hostfile(all_hosts=self.allocated_nodes)
+
+    def generate_hostfile(self):
+        self.hostfile = Hostfile(all_hosts=self.allocated_nodes)
+
+    def validate(self):
+        """Validate that the total nodes requested through the color distribution match num_nodes."""
+        total_nodes_requested = sum(value for value in self.color_distribution.values() if value is not None)
+        if len(self.color_distribution) == 1:
+            # If only one color is used, it's assumed that the entire color will be attributed to num_nodes
+            color = next(iter(self.color_distribution))
+            self.color_distribution[color] = self.num_nodes
+        elif total_nodes_requested != self.num_nodes:
+            raise ValueError(f"Total nodes requested for service {self.name} ({total_nodes_requested}) "
+                             f"does not match num_nodes ({self.num_nodes})")
+
+
+class Architecture:
+    services: dict[str, Service]
+    color_groups: dict[str, int]
+
+    def __init__(self):
+        self.services = dict()
+        self.color_groups = dict()
+
+    def __init__(self, services: list):
+        self.__init__()
+        self.add_service(services)
+
+    def add_service(self, service: Service):
+        if service.name in self.services:
+            raise ValueError("Service already defined {0}".format(service.name))
+        self.services[service.name] = service
+
+    def add_service(self, service_name: str, num_nodes: int, color: str):
+        self.add_service(Service(service_name, num_nodes, color))
+
+    def add_service(self, services: list):
+        for service in services:
+            self.add_service(service)
+
+    def hostfile(self, name: str):
+        return self.services[name].hostfile
+
+    def get_num_nodes_needed(self):
+        for service in self.services.values():
+            for color, num_nodes in service.color_distribution.items():
+                if color in self.color_groups:
+                    if self.color_groups[color] < num_nodes:
+                        self.color_groups[color] = num_nodes
+                else:
+                    self.color_groups[color] = num_nodes
+        return sum(self.color_groups.values())
+
+    def schedule_services(self):
+        total_nodes = self.get_num_nodes_needed()
+        slurm = Slurm(SlurmInfo(nnodes=total_nodes))
+        slurm.allocate()
+        node_list = slurm.get_nodes()
+        if len(node_list) != total_nodes:
+            raise EnvironmentError("Slurm could not reserve enough nodes")
+
+        # Create pools
+        pools = dict()
+        start = 0
+        for color, num_nodes in self.color_groups.items():
+            pools[color] = node_list[start:start + num_nodes]
+            start += num_nodes
+
+        # Assign nodes from pools to services
+        for service in self.services.values():
+            for color, num_nodes in service.color_distribution.items():
+                # Get nodes from color pool
+                pool_nodes = pools[color]
+                service_nodes = pool_nodes[:num_nodes]
+                # Update service with nodes
+                service.add_nodes(service_nodes)
+
+        for service in self.services.values():
+            service.generate_hostfile()
+
+# services = [
+#     Service("OrangeFS_client", 16, "RED:16"),
+#     Service("OrangeFS_server", 16, "BLUE:16"),
+#     Service("Application", 16, "RED:4+BLUE:4+GREEN:8"),
+# ]
+# arch = Architecture(services)
+# arch.schedule_services()
